ba575dd4c751d3abbe5e86f21108f9520dcf9a0e
Ordenkrieger <OrdenKriegerEDR@googlemail.com>
2016-09-25 15:16:26 +0200

    ham: Use bacon GPS "HAX"

--- gps/loc_api/libloc_api_50001/LocEngAdapter.cpp
+++ gps/loc_api/libloc_api_50001/LocEngAdapter.cpp
@@ -375,14 +375,14 @@ void LocEngAdapter::reportPosition(UlpLocation &location,
     }
 }
 
-void LocInternalAdapter::reportSv(QcomGnssSvStatus &svStatus,
+void LocInternalAdapter::reportSv(GnssSvStatxx &svStatus,
                                   GpsLocationExtended &locationExtended,
                                   void* svExt){
     sendMsg(new LocEngReportSv(mLocEngAdapter, svStatus,
                                locationExtended, svExt));
 }
 
-void LocEngAdapter::reportSv(QcomGnssSvStatus &svStatus,
+void LocEngAdapter::reportSv(GnssSvStatxx &svStatus,
                              GpsLocationExtended &locationExtended,
                              void* svExt)
 {
