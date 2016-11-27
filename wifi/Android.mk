a2e171dc2cc40da53095f5d00dffc07d17dd90c8
acuicultor <ramoncuenca4@gmail.com>
2016-11-27 10:25:13 +0100

    ham: Import newest caf prima driver (reverted from commit 7ea00029f760128c2d99f664906ac8a4df64891c)

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
 
-#----------------------------------------------------------------------
-# Copy additional target-specific files
-#----------------------------------------------------------------------
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := wpa_supplicant_overlay.conf
@@ -43,5 +40,4 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd.deny
 include $(BUILD_PREBUILT)
-
 endif
