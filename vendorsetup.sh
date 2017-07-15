681ad232144a5201f5c2c465afde64ddb3e3ca60
sb6596 <shubhamprince111@gmail.com>
2017-07-15 05:57:44 +0000

    ham: mka #Poison

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_ham-user
-add_lunch_combo cm_ham-userdebug
-add_lunch_combo cm_ham-eng
+add_lunch_combo viper_ham-user
+add_lunch_combo viper_ham-userdebug
+add_lunch_combo viper_ham-eng
